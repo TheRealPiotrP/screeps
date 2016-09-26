a1a3834029cc70024816d558e099d934ab805d29
Piotr Puszkiewicz <piotrp@microsoft.com>
2016-09-25 20:26:00 -0700

    Enable code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 'use strict';
 
 var gulp = require('gulp'),
+    istanbul = require('gulp-istanbul'),
     mocha = require('gulp-mocha');
 
-gulp.task('test', function () {
+gulp.task('pre-test', function () {
+  return gulp.src(['roles/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
     return gulp.watch(['roles/**', 'tests/**'], ['test-run']);
 });
 
 gulp.task('test-run', function () {
     return gulp.src('tests/**/*Spec.js', { read: false })
-               .pipe(mocha({ reporter: 'list' }));
+               .pipe(mocha({ reporter: 'list' }))
+               .pipe(istanbul.writeReports({ dir: './.coverage' }));
 });
