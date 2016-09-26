88f4df811962f8aedae57a8ca17478df6bfec4b4
Piotr Puszkiewicz <piotrp@microsoft.com>
2016-09-25 20:01:12 -0700

    harvester coverage improved

--- tests/helpers/any.js
+++ tests/helpers/any.js
@@ -1,6 +1,15 @@
 var any = {
     number: function(min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER){
         return Math.floor((Math.random() * (max - min + 1))) + min;
+    },
+
+    creep: function (){
+        return {
+            carry: {
+                energy: 0
+            },
+            carryCapacity: 0
+        }
     }
 }
 
--- tests/role.harvester.Spec.js
+++ tests/role.harvester.Spec.js
@@ -10,16 +10,29 @@ describe('role.harvester', function() {
     it('is true when energy < capacity', function() {
       var creepCapacity = any.number(0);
 
-      var creep = {
-        carry: {
-          energy: any.number(0, creepCapacity)
-        },
-        carryCapacity: creepCapacity
-      };
+      var creep = any.creep();
+      creep.carry.energy = any.number(0, creepCapacity);
+      creep.carryCapacity = creepCapacity;
 
       var hasCapacity = harvester.hasCapacity(creep);
 
       hasCapacity.should.be.true;
+    });
+  });
+}); 
+
+describe('role.harvester', function() {
+  describe('hasCapacity()', function() {
+    it('is false when energy == capacity', function() {
+      var creepCapacity = any.number(0);
+
+      var creep = any.creep();
+      creep.carry.energy = creepCapacity;
+      creep.carryCapacity = creepCapacity;
+
+      var hasCapacity = harvester.hasCapacity(creep);
+
+      hasCapacity.should.be.false;
     });
   });
 }); 
